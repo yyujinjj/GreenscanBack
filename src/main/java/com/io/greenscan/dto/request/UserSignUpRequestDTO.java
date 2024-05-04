481e096699eaeb0b103a5b3e477c3a0cf24f7d26
Woojung Jung <dnwjd3002@naver.com>
2024-05-05 02:40:06 +0900

    Back_signup_003: SIGNUP_006~007의 기능 구현

--- src/main/java/com/io/greenscan/dto/request/UserSignUpRequestDTO.java
+++ src/main/java/com/io/greenscan/dto/request/UserSignUpRequestDTO.java
@@ -1,5 +1,7 @@
 package com.io.greenscan.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,13 +12,20 @@ public class UserSignUpRequestDTO {
     @NotNull(message = "사용자의 이름은 필수 입력 값 입니다.")
     private String userName;
 
+    @NotNull(message = "비밀번호는 필수 입력 값 입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", message = "대문자 1개, 소문자 1개, 특수문자 1개 이상 포함하세요.")
     private String password;
 
+
+    @NotNull(message = "재확인 비밀번호는 필수 입력 값 입니다.")
     private String confirmPassword;
 
+    @NotNull(message = "이메일은 필수 입력 값 입니다.")
     private String email;
 
+    @NotNull(message="전화번호는 필수 입력 값 입니다.")
     private String phoneNumber;
 
     private String referralId;
 }
+
